3cf11aefcc5663064341354357ffc5debbf9998a
andyRS <andy337@hotmail.es>
2026-01-25 02:13:15 -0400

    Fix menu hover and scroll handling

--- js/mis_script.js
+++ js/mis_script.js
@@ -1,41 +1,42 @@
 $(document).ready(function () {
+  const menu = $(".menu");
+  const menuLinks = $(".menu a");
+
+  function UnderlineHover() {
+    $(this).css("text-decoration", "underline");
+  }
+
+  function NotUnderlineHover() {
+    $(this).css("text-decoration", "none");
+  }
+
+  menuLinks.hover(UnderlineHover, NotUnderlineHover);
+
   $(window).scroll(function () {
     scroll = $(window).scrollTop();
     if (scroll > 685) {
-      var menu = $(".menu");
       menu.css({ position: "fixed" });
       menu.css({ width: "100%" });
       menu.css({ top: "0" });
       menu.css({ background: "#f0637d" });
-      $("menu a").css({ color: "#fff" });
+      menuLinks.css({ color: "#fff" });
       $(".logo").css({ color: "#fff" });
       menu.css({ "box-shadow": "rgba(0, 0, 0, 0.22) 6px 1px 1px" });
       menu.css({ "z-index": "100" });
       $(".contenedor-servicios")
         .css({ width: "90%" })
         .css("margin", "0px auto");
-      function UnderlineHover() {
-        $(this).css("text-decoration", "underline");
-        menu.hover(UnderlineHover);
-      }
     } else {
-      $(".menu").css({ position: "relative" });
-      $(".menu").css({ background: "transparent" });
-      $(".menu").css({ "box-shadow": "0 0 0" });
-      $(".menu a").css({ color: "#fff" });
+      menu.css({ position: "relative" });
+      menu.css({ background: "transparent" });
+      menu.css({ "box-shadow": "0 0 0" });
+      menuLinks.css({ color: "#fff" });
       $(".logo").css({ color: "#fff" });
-
-      function NotUnderlineHover() {
-        $(this).css("text-decoration", "none");
-      }
-      menu.hover(NotUnderlineHover);
     }
 
     $("#php h3").css("color","red").css("display","none");
     if(btn-eviar.click){
       $("#php h3").css("color","red").css("display","block");
-    }else{
-      alert("para enviar el mensaje pulse el boton de enviar mensaje")
     }
     
   });
