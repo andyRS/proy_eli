9663cd7f829650624931c7df6ce0929b8c86eb77
andyRS <andy337@hotmail.es>
2026-01-25 02:13:38 -0400

    Unify entrypoint to index.php

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
           <p class="logo">CONFECCIONES DE INTERIORES ELIZABETH MENDEZ</p>
           <nav>
             <ul>
-              <li><a href="index.html">Inicio</a></li>
+              <li><a href="index.php">Inicio</a></li>
               <li><a href="#">Nuestros Servicios</a></li>
               <li><a href="#">Articulos</a></li>
               <li><a href="#">Quienes Somos</a></li>
